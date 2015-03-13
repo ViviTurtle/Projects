9e539d86261f3408911f0d3bbc285e8a182bc8c5
Anthony Langga <Flippinunit@aim.com>
2015-03-13 01:32:23 -0700

    Removed builditbreakit

--- Competitions/workspace/BuilditBreakit/Decrypt.rb
+++
@@ -1,5 +0,0 @@
-require 'openssl'
-require_relative 'Decryptor'
-
-d = Decryptor.new(ARGV[0], ARGV[1])
-d.read()
--- Competitions/workspace/BuilditBreakit/ruby/Encrypt.rb
+++
@@ -1,5 +0,0 @@
-require 'openssl'
-require_relative 'Encryptor'
-
-e = Encryptor.new(ARGV[0], ARGV[1], ARGV[2])
-e.encrypt()
