f2f802ef7662a609687abeccc99ccc440d011738
Vivi Langga <Flippinunit@aim.com>
2016-01-14 16:00:26 -0800

    Create ModalPopup.cs

---
+++ Code-Snippets/ModalPopup/ModalPopup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+using System.Data.OleDb;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace yourNameSpace
+{
+
+    public partial class yourClass : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        /*
+         * Image Button rebinds data in Modalform
+         */
+        protected void IB_OpenModal_Click(object sender, ImageClickEventArgs e)
+        {
+           
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                lblWarnings.Text = "Error. Please refresh the page.";
+            }
+        }
+    }
+}
+
+
